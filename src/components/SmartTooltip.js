f7f7eecac88a980be3b7c17435f7a83aac291705
ethorz <ethorz.ru@gmail.com>
2020-08-16 13:31:35 +0300

    Update project, update demo

--- src/components/SmartTooltip.js
+++ src/components/SmartTooltip.js
@@ -1,78 +1,92 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import ReactSmartTooltipMethods from './SmartTooltip.local.methods';
 import ReactTooltip from './Tooltip';
 
-class ReactSmartTooltip extends ReactSmartTooltipMethods {
-    static propTypes = {
-        tooltip: PropTypes.shape({
-            title: PropTypes.string,
-            arrow: PropTypes.bool,
-            position: PropTypes.string,
-            className: PropTypes.string
-        }),
-        text: PropTypes.string.isRequired,
-        style: PropTypes.object,
-        className: PropTypes.string,
-        onMouseEnter: PropTypes.func,
-        onShowTooltip: PropTypes.func
+const ReactSmartTooltip = ({ 
+    tooltip = {}, 
+    text, 
+    style, 
+    className, 
+    onMouseEnter = () => {},
+    onShowTooltip = () => {},
+}) => {
+    const [width, setWidth] = useState({
+        offsetWidth: 0,
+        scrollWidth: 0
+    });
+
+    const handleMouseEnter = useCallback((event) => {
+        const { offsetWidth, scrollWidth } = event.target;
+
+        onMouseEnter(event);
+
+        setWidth({
+            offsetWidth,
+            scrollWidth
+        });
+    }, [onMouseEnter]);
+
+    const isShowTooltip = useMemo(() => 
+            width.offsetWidth < width.scrollWidth && !tooltip.disabled, 
+        [width, tooltip]);
+
+    const contentStyles = {
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        ...style
     };
 
-    static defaultProps = {
-        tooltip: {
-            disabled: false
-        },
-        onMouseEnter: () => {},
-        onShowTooltip: () => {}
-    };
+    let content = [
+        <div
+            key="originalContent"
+            style={contentStyles}
+            className={className}
+            onMouseEnter={handleMouseEnter}
+        >
+            {text}
+        </div>
+    ];
 
-    render() {
-        const props = this.props;
-        const isShowTooltip = this.isEllipsisActive() && !props.tooltip.disabled;
-
-        const style = {
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-            overflow: 'hidden',
-            ...props.style
-        };
-
-        let content = [
-            <div
-                key="originalContent"
-                style={style}
-                className={props.className}
-                onMouseEnter={this.onMouseEnter}
-            >
-                {props.text}
-            </div>
-        ];
-
-        if (isShowTooltip) {
-            if (!props.tooltip.title) {
-                props.tooltip.title = props.text;
-            }
-
-            props.onShowTooltip();
-
-            content = [
-                <ReactTooltip
-                    key={'originalDotTooltipWrapper'}
-                    {...props.tooltip}
-                >
-                    <div
-                        className={props.className}
-                        style={style}
-                        onMouseEnter={this.onMouseEnter}
-                    >
-                        {props.text}
-                    </div>
-                </ReactTooltip>
-            ];
+    if (isShowTooltip) {
+        if (!tooltip.title) {
+            tooltip.title = text;
         }
 
-        return content;
-    }
-}
+        onShowTooltip();
 
-export default ReactSmartTooltip
+        content = [
+            <ReactTooltip
+                key={'originalDotTooltipWrapper'}
+                {...tooltip}
+            >
+                <div
+                    className={className}
+                    style={contentStyles}
+                    onMouseEnter={handleMouseEnter}
+                >
+                    {text}
+                </div>
+            </ReactTooltip>
+        ];
+    }
+
+    return content;
+};
+
+ReactSmartTooltip.propTypes = {
+    tooltip: PropTypes.shape({
+        title: PropTypes.string,
+        arrow: PropTypes.bool,
+        position: PropTypes.string,
+        className: PropTypes.string,
+        disabled: PropTypes.bool
+    }),
+    text: PropTypes.string.isRequired,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    onMouseEnter: PropTypes.func,
+    onShowTooltip: PropTypes.func
+};
+
+export default ReactSmartTooltip;
